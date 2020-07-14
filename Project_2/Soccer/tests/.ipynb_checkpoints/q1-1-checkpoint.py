5dc888c1a179707d2db9ada4b02b7a6ecce6c4e9
Default Jupyter user <alleanna@berkeley.edu>
2020-07-13 22:53:06 -0700

    EPA

---
+++ Project_2/Soccer/tests/.ipynb_checkpoints/q1-1-checkpoint.py
@@ -0,0 +1,16 @@
+test = {
+    'name' : '1.1',
+    'suites' : [{
+            'cases' : [{
+                'code' : r"""
+                >>> num_columns == 28
+                True
+                """
+            }, {
+                'code' : r"""
+                >>> num_rows == 115457
+                True
+                """
+            }]
+        }]
+}
